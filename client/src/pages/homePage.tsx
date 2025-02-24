39c155983a758eac20c79dfa8f4a48ae0202704d
Anusree <anilkumaranusree113@gmail.com>
2025-02-24 22:40:46 +0530

    fix(client): homePage

--- client/src/pages/homePage.tsx
+++ client/src/pages/homePage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import "../styles/home-page.scss";
 import { signup } from "../api/signup";
 import { useNavigate } from "react-router-dom";
