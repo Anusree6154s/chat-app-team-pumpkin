d025d032b89cabd9d33c74770b0301104fe8f173
Anusree <anilkumaranusree113@gmail.com>
2025-02-24 22:50:04 +0530

    fix(client): added serevr url

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -1,6 +1,11 @@
 import axios from "axios";
 
+// export const api = axios.create({
+//   baseURL: "http://localhost:8000/api",
+//   headers: { "Content-Type": "application/json" },
+// });
+
 export const api = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "https://chat-app-team-pumpkin-server.vercel.app/",
   headers: { "Content-Type": "application/json" },
 });
